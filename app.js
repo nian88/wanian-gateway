a28c1d9ae0698fd0bb3298e356d12131d10aff5d
Azhar Nian <azharnian@gmail.com>
2022-04-13 07:46:09 +0700

    [update] model auth

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const { Client, MessageMedia } = require('whatsapp-web.js');
+const { Client, MessageMedia, LocalAuth } = require('whatsapp-web.js');
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
 const fs = require('fs');
-const axios = require('axios');
-const { body, validationResult } = require('express-validator');
-const { response } = require('express');
-const fileUpload = require('express-fileupload');
 const { phoneNumberFormatter } = require('./helpers/formatter');
-const db = require('./helpers/mysqldb');
-const api = require('./helpers/api');
+const fileUpload = require('express-fileupload');
+const axios = require('axios');
+const mime = require('mime-types');
+
+//const db = require('./helpers/mysqldb');
+//const api = require('./helpers/api');
 const port = process.env.PORT || 3000;
 const host = process.env.HOST ||"0.0.0.0";
 
 const app = express();
 const server = http.createServer(app);
-// const io = socketIO(server);
 const io = socketIO(server, {
   cors: {
     origin: '*',
@@ -27,19 +27,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 app.use(fileUpload({debug: true }));
 
-const SESSION_FILE_PATH = './session.json';
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
-
-
-app.get('/', (req, res)=>{
-    res.sendFile('index.html', {root: __dirname});
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname
+  });
 });
 
-
 const client = new Client({
+    restartOnAuthFail: true,
     puppeteer: {
         headless: true,
         args: [
@@ -53,20 +48,8 @@ const client = new Client({
           '--disable-gpu'
         ],
     },
-    session: sessionCfg
+    authStrategy: new LocalAuth()
 });
-
-
-client.on('auth_failure', msg => {
-    console.error('AUTHENTICATION FAILURE', msg);
-});
-
-client.on('message', async msg => {
-    const phone = (await msg.getContact()).number;
-    await api.sendToInbox(phone, msg.body);
-});
-
-
 
 client.initialize();
 
@@ -81,22 +64,27 @@ io.on('connection', function(socket){
         });
     });
 
-    client.on('authenticated', (session) => {
-
-        socket.emit('authenticated', 'Whatsapp Is Authenticated');
-        socket.emit('message', 'Whatsapp Is Authenticated');
-        sessionCfg=session;
-        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-            if (err) {
-                console.error(err);
-            }
-        });
-    });
-
     client.on('ready', () => {
         socket.emit('message', 'Whatsapp Is Ready');
         console.log('Ready');
     });
+
+    client.on('authenticated', () => {
+        socket.emit('authenticated', 'Whatsapp Is Authenticated');
+        socket.emit('message', 'Whatsapp Is Authenticated');
+        console.log('Authenticated');
+    });
+
+    client.on('auth_failure', msg => {
+        console.error('AUTHENTICATION FAILURE', msg);
+    });
+
+    client.on('disconnected', (reason) => {
+        socket.emit('message', 'Whatsapp is disconnected!');
+        console.error('Disconnected', reason);
+        client.destroy();
+        client.initialize();
+      });
 });
 
 const checkRegisteredNumber = async function(number) {
